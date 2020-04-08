ad49e754b358136a37ef29b827fc166db3fa3f90
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-07 18:37:34 -0700

    main class added

---
+++ src/superherouniverse/StartGame.java
@@ -0,0 +1,7 @@
+package superherouniverse;
+
+public class StartGame {
+    public static void main(String[] args) {
+        System.out.println("This is not the finished product. This is just a tribute.");
+    }
+}
