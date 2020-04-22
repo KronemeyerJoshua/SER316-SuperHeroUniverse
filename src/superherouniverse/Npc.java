9e2329c13262bde9f97aa91e4ae7ac7958d4c063
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 01:29:25 -0700

    Added more getters/setters for health, name, abilities

--- src/superherouniverse/Npc.java
+++ src/superherouniverse/Npc.java
@@ -1,17 +1,36 @@
 package superherouniverse;
 
+import java.util.Vector;
+
 abstract class Npc {
     private String name;
-    private int health;
+    protected int health;
 
     static Npc create(NpcClass n) {
         switch (n) {
-            case VILLIAN:
-                return new Villian();
-                break;
+            case VILLAIN:
+                return new Villain();
             case HERO:
                 return new Hero();
-                break;
         }
+        return null;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void damageHealth(int damage) {
+        this.health -= damage;
+    }
+
+    abstract Vector<Ability> getAbilities();
 }
