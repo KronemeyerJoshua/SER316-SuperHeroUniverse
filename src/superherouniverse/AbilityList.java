46ef4ac115d9f424c6fc110caa82eadfe7e06458
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 11:59:11 -0700

    Implementation of loading abilities from JSONFile

--- src/superherouniverse/AbilityList.java
+++ src/superherouniverse/AbilityList.java
@@ -1,21 +1,49 @@
 package superherouniverse;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Vector;
 
 public final class AbilityList {
-    public static final List<Ability> HeroAbilities = Collections.unmodifiableList(
-            new ArrayList<Ability>() {{
-                add(new Ability(10, "SLAP YO FACE", "SLAP"));
-            }});
+    public static final Vector<Ability> heroAbilities;
+    public static final Vector<Ability> villainAbilities;
 
-    public static final List<Ability> VillianAbilities = Collections.unmodifiableList(
-            new ArrayList<Ability>() {{
-                add(new Ability(8, "PICKPOCKET", "PICKPOCKET"));
-            }});
+    /*
+     * Loads the AbilityList for heroes and villains
+     */
+    static {
+        // Get our abilities from local game data
+        JSONObject abilities = null;
+        try {
+            abilities = new JSONObject(FileOperations.readFile(FileOperations.abilityDataLoc));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Temp Vectors
+        Vector<Ability> heroAbilitiesBuilder = new Vector<>();
+        Vector<Ability> villainAbilitiesBuilder = new Vector<>();
 
-    private AbilityList() {
+        // Get our hero abilities and make them into Ability
+        JSONArray abilityArray = abilities.getJSONArray("heroAbilities");
+        // System.out.println(abilityArray.getJSONObject(0).toString());
+        for (int i = 0; i < abilityArray.length(); i++) {
+            JSONObject ability = abilityArray.getJSONObject(i);
+            heroAbilitiesBuilder.add(new Ability(ability.getInt("damage"), ability.getString("description"), ability.getString("name")));
+        }
+
+        // Get our villain abilities and make them into Ability
+        abilityArray = abilities.getJSONArray("villainAbilities");
+        for (int i = 0; i < abilityArray.length(); i++) {
+            JSONObject ability = abilityArray.getJSONObject(i);
+            villainAbilitiesBuilder.add(new Ability(ability.getInt("damage"), ability.getString("description"), ability.getString("name")));
+        }
+
+
+        // Initialize our static properties
+        heroAbilities = heroAbilitiesBuilder;
+        villainAbilities = villainAbilitiesBuilder;
     }
 
 }
