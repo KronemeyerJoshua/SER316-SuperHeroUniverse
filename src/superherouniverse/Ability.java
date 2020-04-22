15ecae8678775eccd04e150a4e60225ce0fce548
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 01:26:43 -0700

    Basic Ability constructor, getters, setters

--- src/superherouniverse/Ability.java
+++ src/superherouniverse/Ability.java
@@ -2,5 +2,35 @@ package superherouniverse;
 
 public class Ability {
     private int damage;
-    private String description;
+    private String description, name;
+
+    public Ability(int damage, String description, String name) {
+        this.damage = damage;
+        this.description = description;
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
