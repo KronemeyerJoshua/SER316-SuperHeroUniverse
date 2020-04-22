cefb3dc3fc7af1fe26d073bc56f16311ce5deed4
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 01:28:19 -0700

    Test hero implementation, static values need to be changed

--- src/superherouniverse/Hero.java
+++ src/superherouniverse/Hero.java
@@ -1,4 +1,18 @@
 package superherouniverse;
 
+import java.util.Vector;
+
 public class Hero extends Npc {
+    Vector<Ability> abilities;
+    public Hero() {
+        abilities = new Vector<>();
+        abilities.add(new Ability(10, "HolyNova", "HolyNova"));
+        super.setName("HERO");
+        super.health = 100;
+    }
+
+    @Override
+    public Vector<Ability> getAbilities() {
+        return abilities;
+    }
 }
