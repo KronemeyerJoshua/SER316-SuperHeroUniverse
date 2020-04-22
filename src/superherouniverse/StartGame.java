093d71f93d259633595d4cef38b31c6c554bf7f4
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 01:29:52 -0700

    Basic test of the combat simulation

--- src/superherouniverse/StartGame.java
+++ src/superherouniverse/StartGame.java
@@ -1,7 +1,24 @@
 package superherouniverse;
 
+import java.util.Vector;
+
 public class StartGame {
     public static void main(String[] args) {
-        System.out.println("This is not the finished product. This is just a tribute.");
+        Vector<Npc> heroes = new Vector<>();
+        Vector<Npc> villains = new Vector<>();
+        for (int i = 0; i < 5; i++) {
+            heroes.add(Npc.create(NpcClass.HERO));
+            villains.add(Npc.create(NpcClass.VILLAIN));
+        }
+
+        Combat combat = new Combat(heroes, villains);
+        boolean b = combat.runCombatSim();
+
+        if (b) {
+            System.out.println("Heroes win!");
+        }
+        else {
+            System.out.println("Villians Win!");
+        }
     }
 }
