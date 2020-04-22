9c51a5d1d10c2655605db7dd7bda7d2f92e01d83
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 14:58:03 -0700

    Modified to grab a villian and ability from villian pool

--- src/superherouniverse/Villain.java
+++ src/superherouniverse/Villain.java
@@ -7,14 +7,28 @@ public class Villain extends Npc {
     Vector<Ability> abilities;
     public Villain() {
         Random rnd = new Random();
+        Npc v = VillainList.villainPool.get(rnd.nextInt(VillainList.villainPool.size()));
+        super.setName(v.getName());
+        super.health = v.getHealth();
+        abilities = new Vector<>(v.getAbilities());
+    }
+
+    public Villain(String name, int health) {
+        Random rnd = new Random();
+        super.setName(name);
+        super.health = health;
         abilities = new Vector<>();
         abilities.add(AbilityList.villainAbilities.get(rnd.nextInt(AbilityList.villainAbilities.size())));
-        super.setName("VILLAIN");
-        super.health = 100;
     }
 
     @Override
     public Vector<Ability> getAbilities() {
         return abilities;
     }
+
+    @Override
+    public Npc spawnFrom() {
+        return null;
+    }
+
 }
