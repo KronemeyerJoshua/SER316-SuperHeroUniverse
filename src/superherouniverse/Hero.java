7a2129c00844d5344955fa7a79bf5fc0f89989e4
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 14:55:33 -0700

    Modified hero to get hero from hero pool when called

--- src/superherouniverse/Hero.java
+++ src/superherouniverse/Hero.java
@@ -7,14 +7,31 @@ public class Hero extends Npc {
     Vector<Ability> abilities;
     public Hero() {
         Random rnd = new Random();
+        Npc h = HeroList.heroPool.get(rnd.nextInt(HeroList.heroPool.size()));
+        super.setName(h.getName());
+        super.health = h.getHealth();
+        abilities = new Vector<>(h.getAbilities());
+    }
+
+    public Hero(String name, int health) {
+        Random rnd = new Random();
+        super.setName(name);
+        super.health = health;
         abilities = new Vector<>();
         abilities.add(AbilityList.heroAbilities.get(rnd.nextInt(AbilityList.heroAbilities.size())));
-        super.setName("HERO");
-        super.health = 100;
     }
 
     @Override
     public Vector<Ability> getAbilities() {
         return abilities;
+    }
+
+    @Override
+    public Npc spawnFrom() {
+        return null;
+    }
+
+    public String toString() {
+        return String.format("Name: %s Health %s", super.getName(), super.getHealth());
     }
 }
