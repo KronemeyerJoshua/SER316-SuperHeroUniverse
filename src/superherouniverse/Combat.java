aed210d461e3273b2dccb2a533a40abeea5d2727
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 23:21:48 -0700

    Added JavaDoc comments, removed old team fighting code

--- src/superherouniverse/Combat.java
+++ src/superherouniverse/Combat.java
@@ -4,55 +4,15 @@ import java.util.Random;
 import java.util.Vector;
 
 public class Combat {
-    Vector<Npc> heroes;
-    Vector<Npc> villians;
     Npc hero;
     Npc villain;
 
-    public Combat() {};
+    public Combat() {}
 
-    public Combat(Npc hero, Npc villain) {
-        this.hero = hero;
-        this.villain = villain;
-    }
-
-    public Combat(Vector<Npc> heroes, Vector<Npc> villians) {
-        this.heroes = heroes;
-        this.villians = villians;
-    }
-
-    public boolean runCombatTeamSim() {
-        boolean heroesWin = true;
-
-        while (heroes.size() > 0 && villians.size() > 0) {
-            attackTarget(heroes, villians);
-            attackTarget(villians, heroes);
-        }
-
-        if (villians.size() > 0)
-            heroesWin = false;
-
-        return heroesWin;
-    }
-
-    public void attackTarget(Vector<Npc> attackers, Vector<Npc> targets) {
-        Random rnd = new Random();
-        for (int i = 0; i < attackers.size(); i++) {
-            Vector<Ability> abilities = attackers.get(i).getAbilities();
-            Ability chosenAbility = abilities.get(rnd.nextInt(abilities.size()));
-            Npc target = targets.get(rnd.nextInt(targets.size()));
-            target.damageHealth(chosenAbility.getDamage());
-            // System.out.printf("%s has hit %s for %d damage%n", attackers.get(i).getName(), target.getName(), chosenAbility.getDamage());
-            if (target.getHealth() < 1) {
-                // System.out.printf("%s has fainted.%n", target.getName());
-                targets.remove(target);
-                if (targets.size() < 1) {
-                    break;
-                }
-            }
-        }
-    }
-
+    /**
+     * Runs a combat simulation between hero & villain.
+     * @return Boolean if heroes have won
+     */
     public boolean runCombatSim() {
         boolean heroesWin = true;
 
@@ -61,20 +21,34 @@ public class Combat {
             attackSingleTarget(villain, hero);
         }
 
-        if (villain.getHealth() > 0)
+        if (villain.getHealth() > 0) {
             heroesWin = false;
+        }
 
         return heroesWin;
     }
 
+    /**
+     * A single attack to a target.
+     * @param attacker Npc attacker
+     * @param target Npc targeted
+     */
     public void attackSingleTarget(Npc attacker, Npc target) {
         Random rnd = new Random();
-            Vector<Ability> abilities = attacker.getAbilities();
-            Ability chosenAbility = abilities.get(rnd.nextInt(abilities.size()));
-            target.damageHealth(chosenAbility.getDamage());
-            // System.out.printf("%s has hit %s for %d damage%n", attackers.get(i).getName(), target.getName(), chosenAbility.getDamage());
+        Vector<Ability> abilities = attacker.getAbilities();
+        Ability chosenAbility = abilities.get(rnd.nextInt(abilities.size()));
+        int damage = chosenAbility.getDamage();
+        if (rnd.nextDouble() < Stats.VILLIAN_CRIT_CHANCE) {
+            damage *= 2;
+        }
+        target.damageHealth(damage);
     }
 
+    /**
+     * Replaces the npcs in the combat simulation.
+     * @param hero The replacement hero
+     * @param villain The replacement villain
+     */
     public void replaceNpcs(Npc hero, Npc villain) {
         this.hero = hero;
         this.villain = villain;
