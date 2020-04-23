c81633e5688a91989a1c8ae2046ece3fe3bb11ed
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 18:45:59 -0700

    Temp stats file, need to create json/import stats from json

--- src/superherouniverse/Stats.java
+++ src/superherouniverse/Stats.java
@@ -1,18 +1,15 @@
 package superherouniverse;
 
-public enum Stats {
-    VILLIAN_SPAWN_CHANCE(0.3),
-    HERO_BORN_CHANCE(0.2),
-    VILLIAN_CRIT_CHANCE(0.2),
-    HERO_CRIT_CHANCE(0.6);
+public final class Stats {
+    final static double VILLIAN_SPAWN_CHANCE;
+    final static double HERO_BORN_CHANCE;
+    final static double VILLIAN_CRIT_CHANCE;
+    final static double HERO_CRIT_CHANCE;
 
-    private double chance;
-
-    public double getChance() {
-        return this.chance;
-    }
-
-    Stats(double chance) {
-        this.chance = chance;
+    static {
+        VILLIAN_SPAWN_CHANCE = 0.3;
+        HERO_BORN_CHANCE = 0.2;
+        VILLIAN_CRIT_CHANCE = 0.2;
+        HERO_CRIT_CHANCE = 0.6;
     }
 }
