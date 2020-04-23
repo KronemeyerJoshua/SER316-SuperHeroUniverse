3f0f4b707b7fdb73ec3a07d747e57cbf77f2cdb3
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 23:40:07 -0700

    Comments detailing game loop

--- src/superherouniverse/StartGame.java
+++ src/superherouniverse/StartGame.java
@@ -13,29 +13,37 @@ public class StartGame {
         Vector<Location> fallenLocations = new Vector<>();
         Vector<Location> enlightenedLocations = new Vector<>();
 
+        // Pull 5 random locations to use
         System.out.printf("Generating locations...%n");
         for (int i = 0; i < 5; i++) {
             locations.add(new Location());
         }
 
+        // Generate 5 heroes/villains, put them in bases
         System.out.printf("Generating bases and putting people inside them...%n");
-        for (Location value : locations) {
+        for (Location loc : locations) {
             Vector<Npc> heroes = new Vector<>();
             Vector<Npc> villains = new Vector<>();
             for (int j = 0; j < 5; j++) {
                 heroes.add(Npc.create(NpcClass.HERO));
                 villains.add(Npc.create(NpcClass.VILLAIN));
             }
-            value.addBase(new Base(heroes, NpcClass.HERO));
-            value.addBase(new Base(villains, NpcClass.VILLAIN));
+            // Put our bases in our locations
+            loc.addBase(new Base(heroes, NpcClass.HERO));
+            loc.addBase(new Base(villains, NpcClass.VILLAIN));
         }
 
+        // Need random to generate number for encounter chance
         Random rnd = new Random();
         Combat combat = new Combat();
 
+        // We remove locations when they've fallen/enlightened
         while (locations.size() > 0) {
+            // Iterate throuigh each location
             for (Location location : locations) {
                 System.out.printf("Checking in on %s%n", location.getName());
+
+                // If random rolls within the encounter chance, hero has met villain
                 if (rnd.nextDouble() <= location.getEncounterChance()) {
                     Npc villain = location.getVillainBase().getRandomNpc();
                     Npc hero = location.getHeroBase().getRandomNpc();
@@ -45,18 +53,23 @@ public class StartGame {
                     combat.replaceNpcs(hero, villain);
                     boolean b = combat.runCombatSim();
 
+                    // If b is true, the hero has won
                     if (b) {
+                        // Get first ability of villain and add it to hero
                         hero.addAbility(villain.getAbilities().get(0));
+                        System.out.printf("%s triumphed and obtained the power %s%n",
+                                hero.getName(), villain.getAbilities().get(0).getName());
                         location.getVillainBase().removeNpc(villain);
-                        System.out.printf("%s triumphed and obtained the power%n",
-                                hero.getName());
                     } else {
+                        // Get first ability of hero and add it to villain
                         villain.addAbility(hero.getAbilities().get(0));
+                        System.out.printf("%s triumphed and obtained the power %s%n",
+                                villain.getName(), hero.getAbilities().get(0).getName());
                         location.getHeroBase().removeNpc(hero);
-                        System.out.printf("%s triumphed and obtained the power%n",
-                                villain.getName());
                     }
 
+                    // Check to see if any locations have
+                    // fallen or been enlightened
                     if (location.getHeroBase().isEmpty()) {
                         location.setCorrupted(true);
                         fallenLocations.add(location);
@@ -68,15 +81,19 @@ public class StartGame {
                 }
             }
 
+            // If location has fallen, remove it
             for (Location location : fallenLocations) {
                 locations.remove(location);
             }
 
+            // If location has been enlightened, remove it
             for (Location location : enlightenedLocations) {
                 locations.remove(location);
             }
         }
 
+        // All locations have been battled for
+        // Determine ending
         if (fallenLocations.size() > enlightenedLocations.size()) {
             System.out.printf("And so the world turned to darkness as the majority"
                     + " of the planet is now ruled by villains.%n");
