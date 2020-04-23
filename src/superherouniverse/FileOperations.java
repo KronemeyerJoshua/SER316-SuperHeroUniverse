eaae31a5f292779ef08e10397912fc4259ea8c13
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 23:22:05 -0700

    Added UTF-8 Characterization for file reader

--- src/superherouniverse/FileOperations.java
+++ src/superherouniverse/FileOperations.java
@@ -3,6 +3,7 @@ package superherouniverse;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public final class FileOperations {
 
@@ -18,14 +19,21 @@ public final class FileOperations {
         locationDataLoc = "_data/locations.json";
     }
 
+    /**
+     * Reads the file given and returns a string of contents.
+     * @param filename File path
+     * @return String of contents
+     * @throws IOException fileNotFound
+     */
     public static String readFile(String filename) throws IOException {
         StringBuilder loadData = new StringBuilder();
         String s;
-        BufferedReader fileInput = new BufferedReader(new FileReader(filename));
+        BufferedReader fileInput = new BufferedReader(new FileReader(filename, StandardCharsets.UTF_8));
         while ((s = fileInput.readLine()) != null) {
             loadData.append(s);
         }
 
+        fileInput.close();
         return loadData.toString();
     }
 }
