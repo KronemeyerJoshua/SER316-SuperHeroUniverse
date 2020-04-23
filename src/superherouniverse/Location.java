ae69656a83f42d01d80a4d2cad4e8f8edb56329b
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 18:44:42 -0700

    Added encounter chance and bases vector

--- src/superherouniverse/Location.java
+++ src/superherouniverse/Location.java
@@ -1,11 +1,22 @@
 package superherouniverse;
 
+import java.util.Random;
 import java.util.Vector;
 
 public class Location {
     private String name;
     private Vector<Base> bases;
     private boolean isCorrupted;
+    private double encounterChance;
+
+    public Location() {
+        Random rnd = new Random();
+        Location l = LocationList.locationPool.get(rnd.nextInt(LocationList.locationPool.size()));
+        bases = new Vector<>();
+        this.name = l.getName();
+        this.isCorrupted = l.isCorrupted();
+        this.encounterChance = l.getEncounterChance();
+    }
 
     public Location(String name, Vector<Base> bases, boolean isCorrupted) {
         this.name = name;
@@ -13,9 +24,10 @@ public class Location {
         this.isCorrupted = isCorrupted;
     }
 
-    public Location(String name, boolean isCorrupted) {
+    public Location(String name, boolean isCorrupted, double encounterChance) {
         this.name = name;
         this.isCorrupted = isCorrupted;
+        this.encounterChance = encounterChance;
     }
 
     public String getName() {
@@ -40,5 +52,34 @@ public class Location {
 
     public void removeBase(Base b) {
         bases.remove(b);
+    }
+
+    public boolean addBase(Base b) {
+        boolean success = false;
+        if (bases.size() < 2) {
+            bases.add(b);
+            success = true;
+        }
+        return success;
+    }
+
+    public double getEncounterChance() {
+        return encounterChance;
+    }
+
+    public Base getVillainBase() {
+        for (Base b : bases) {
+            if (b.type == NpcClass.VILLAIN)
+                return b;
+        }
+        return null;
+    }
+
+    public Base getHeroBase() {
+        for (Base b : bases) {
+            if (b.type == NpcClass.HERO)
+                return b;
+        }
+        return null;
     }
 }
