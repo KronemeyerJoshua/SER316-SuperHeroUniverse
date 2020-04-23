97595becf8ce8d2e71e5e8c73babd56dd6a7308a
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 23:23:45 -0700

    Main loop of game added

--- src/superherouniverse/StartGame.java
+++ src/superherouniverse/StartGame.java
@@ -4,9 +4,14 @@ import java.util.Random;
 import java.util.Vector;
 
 public class StartGame {
-    public static void main(String[] args)
-    {
+    /**
+     * Main game loop/setup.
+     * @param args Console args
+     */
+    public static void main(String[] args) {
         Vector<Location> locations = new Vector<>();
+        Vector<Location> fallenLocations = new Vector<>();
+        Vector<Location> enlightenedLocations = new Vector<>();
 
         System.out.printf("Generating locations...%n");
         for (int i = 0; i < 5; i++) {
@@ -27,19 +32,61 @@ public class StartGame {
 
         Random rnd = new Random();
         Combat combat = new Combat();
-        for (Location location : locations) {
-            System.out.printf("Checking in on %s%n", location.getName());
-            if (rnd.nextDouble() <= location.getEncounterChance()) {
-                System.out.printf("Encounter happening %s%n", location.getName());
-                Vector<Base> bases = location.getBases();
-                Npc villain = location.getVillainBase().getRandomNpc();
-                Npc hero = location.getHeroBase().getRandomNpc();
-                combat.replaceNpcs(hero, villain);
-                boolean b = combat.runCombatSim();
-                if (b) {
-                    System.out.println("hero win");
+
+        while (locations.size() > 0) {
+            for (Location location : locations) {
+                System.out.printf("Checking in on %s%n", location.getName());
+                if (rnd.nextDouble() <= location.getEncounterChance()) {
+                    Npc villain = location.getVillainBase().getRandomNpc();
+                    Npc hero = location.getHeroBase().getRandomNpc();
+
+                    System.out.printf("Hero %s is battling Villain %s%n",
+                            hero.getName(), villain.getName());
+                    combat.replaceNpcs(hero, villain);
+                    boolean b = combat.runCombatSim();
+
+                    if (b) {
+                        hero.addAbility(villain.getAbilities().get(0));
+                        location.getVillainBase().removeNpc(villain);
+                        System.out.printf("%s triumphed and obtained the power%n",
+                                hero.getName());
+                    } else {
+                        villain.addAbility(hero.getAbilities().get(0));
+                        location.getHeroBase().removeNpc(hero);
+                        System.out.printf("%s triumphed and obtained the power%n",
+                                villain.getName());
+                    }
+
+                    if (location.getHeroBase().isEmpty()) {
+                        location.setCorrupted(true);
+                        fallenLocations.add(location);
+                    } else if (location.getVillainBase().isEmpty()) {
+                        enlightenedLocations.add(location);
+                    }
+                } else {
+                    System.out.printf("\tIt's quiet...%n");
                 }
             }
+
+            for (Location location : fallenLocations) {
+                locations.remove(location);
+            }
+
+            for (Location location : enlightenedLocations) {
+                locations.remove(location);
+            }
+        }
+
+        if (fallenLocations.size() > enlightenedLocations.size()) {
+            System.out.printf("And so the world turned to darkness as the majority"
+                    + " of the planet is now ruled by villains.%n");
+        } else if (fallenLocations.size() < enlightenedLocations.size()) {
+            System.out.printf("And so the world reached a new age of enlightenment"
+                    + " as the majority of the planet is now helped by heroes.%n");
+        } else {
+            System.out.printf("The war raged on for many years. Cities burned, "
+                    + "innocent civilians dead, but there is no end in sight for the "
+                    + "battle between heroes and villains.%n");
         }
     }
 
