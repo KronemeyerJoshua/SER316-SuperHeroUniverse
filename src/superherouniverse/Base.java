5531b523f75999876c8845e7243e60275f9fab86
KronemeyerJoshua <jkroneme@asu.edu>
2020-04-22 23:21:28 -0700

    Added removeNpc, getType, isEmpty

--- src/superherouniverse/Base.java
+++ src/superherouniverse/Base.java
@@ -4,25 +4,28 @@ import java.util.Random;
 import java.util.Vector;
 
 public class Base {
-    Vector<Npc> npcs;
-    NpcClass type;
+    private Vector<Npc> npcs;
+    private NpcClass type;
 
     public Base(Vector<Npc> npcs, NpcClass type) {
         this.npcs = new Vector<>(npcs);
         this.type = type;
     }
 
-    public boolean addNpc(Npc npc) {
-        boolean success = false;
-        if (npcs.size() < 5) {
-            npcs.add(npc);
-            success = true;
-        }
-        return success;
-    }
-
     public Npc getRandomNpc() {
         Random rnd = new Random();
         return npcs.get(rnd.nextInt(npcs.size()));
+    }
+
+    public void removeNpc(Npc npc) {
+        npcs.remove(npc);
+    }
+
+    public NpcClass getType() {
+        return type;
+    }
+
+    public boolean isEmpty() {
+        return npcs.isEmpty();
     }
 }
